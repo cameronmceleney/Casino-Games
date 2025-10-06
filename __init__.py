e3d35a67c84571a1b938772d5f5c8a409e8b3a1a
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2025-10-06 15:50:06 +0100

    Initial upload of files.

---
+++ __init__.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""(One liner introducing this file init.py)
+
+(
+Leading paragraphs explaining file in more detail.
+)
+
+Attributes:
+    (Here, place any module-scope constants users will import.)
+    
+Constants:
+    MODULE_LEVEL_CONSTANT1 (int): A module-level constant.
+
+Examples:
+    (Here, place useful implementations of the contents of init.py). Note that leading symbol '>>>' includes the
+    code in doctests, while '$' does not.)::
+
+(
+Trailing paragraphs summarising final details.
+)
+
+Todo:
+    * (Optional section for module-wide tasks).
+    * (Use format: 'YYMMDD/task_identifier - one-liner task description'
+    
+References:
+    Style guide: `Google Python Style Guide`_
+
+Notes:
+    File version
+        0.1.0
+    Project
+        Keno-Game
+    Path
+        src/init.py
+    Author
+        Cameron Aidan McEleney < c.mceleney.1@research.gla.ac.uk >
+    Created
+        06 Oct 2025
+    IDE
+        PyCharm
+        
+.. _Google Python Style Guide:
+   https://google.github.io/styleguide/pyguide.html
+"""
+
+# Local application imports
+from src.keno import Keno
+
+# Module-level constants
+__version__ = "1.0.0"
+"""Release version."""
---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""(One liner introducing this file main.py)
+
+(
+Leading paragraphs explaining file in more detail.
+)
+
+Attributes:
+    (Here, place any module-scope constants users will import.)
+    
+Constants:
+    MODULE_LEVEL_CONSTANT1 (int): A module-level constant.
+
+Examples:
+    (Here, place useful implementations of the contents of main.py). Note that leading symbol '>>>' includes the 
+    code in doctests, while '$' does not.)::
+
+(
+Trailing paragraphs summarising final details.
+)
+
+Todo:
+    * (Optional section for module-wide tasks).
+    * (Use format: 'YYMMDD/task_identifier - one-liner task description'
+    
+References:
+    Style guide: `Google Python Style Guide`_
+
+Notes:
+    File version
+        0.1.0
+    Project
+        Keno-Game
+    Path
+        /main.py
+    Author
+        Cameron Aidan McEleney < c.mceleney.1@research.gla.ac.uk >
+    Created
+        06 Oct 2025
+    IDE
+        PyCharm
+        
+.. _Google Python Style Guide:
+   https://google.github.io/styleguide/pyguide.html
+"""
+
+# Local application imports
+from src.casino import Casino
+
+
+if __name__ == "__main__":
+    casino = Casino(game="keno", slip_name="keno-slip.yml")
+    casino.play()
+
+
